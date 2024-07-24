753a7c48cd4a4ad9d3c6dceb623abb94da432b6a
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2024-07-23 22:48:06 -0300

    Time: 144 ms (60.62%), Space: 114.2 MB (52.85%) - LeetHub

---
+++ 2670-make-k-subarray-sums-equal/2670-make-k-subarray-sums-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long makeSubKSumEqual(vector<int>& arr, int k) {
+        long long ans = 0;
+        int n = arr.size();
+        k = __gcd(k, n);
+        for (int i = 0; i < k; i++) {
+            vector<int> v;
+            for (int j = i; j < n; j += k) {
+                v.push_back(arr[j]);
+            }
+            sort(v.begin(), v.end());
+            int median = v[v.size() / 2];
+            for (int j = i; j < n; j += k) {
+                ans += abs(median - arr[j]);
+            }
+        }
+        return ans;
+    }
+};
