010c36216be93b73d8c67c77b4d38b1917c6b409
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:52:15 +0530

    Create 2536-DetermineIfTwoEventsHaveConflict

---
+++ 2536-DetermineIfTwoEventsHaveConflict/2536-DetermineIfTwoEventsHaveConflict.cpp
@@ -0,0 +1,7 @@
+// Last updated: 1/22/2026, 7:52:15 PM
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+          return event2[0] <= event1[1] and event2[1] >= event1[0];
+    }
+    };
