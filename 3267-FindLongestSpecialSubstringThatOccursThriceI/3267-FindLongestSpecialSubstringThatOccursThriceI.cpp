55526d4b6f67ede9d27c98c5b0003303487909c9
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:50:59 +0530

    Create 3267-FindLongestSpecialSubstringThatOccursThriceI

---
+++ 3267-FindLongestSpecialSubstringThatOccursThriceI/3267-FindLongestSpecialSubstringThatOccursThriceI.cpp
@@ -0,0 +1,30 @@
+// Last updated: 1/22/2026, 7:50:59 PM
+class Solution {
+private:
+    bool special(string &s,int i,int j){
+    while(i<j){
+        if(s[i]!=s[i+1]) return false;
+        i++;
+    }
+    return true;
+}
+public:
+    int maximumLength(string s) {
+    int i,j;
+    map<string,int> count;
+    for(i=0;i<s.length();i++){
+        for(j=i;j<s.length();j++){
+            if(special(s,i,j)){
+                count[s.substr(i,j-i+1)]++;
+            }
+        }
+    }
+    int ans=0;
+    for(auto i : count){
+        string str=i.first;
+        if(i.second>=3 && str.length()>ans) ans=str.length();
+    }
+        if(ans==0) return -1;
+        return ans;
+    }
+};
