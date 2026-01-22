050d7ad79b3466718e64bb17a6ffec4d56333ed4
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:40 +0530

    Create 565-ArrayNesting

---
+++ 565-ArrayNesting/565-ArrayNesting.cpp
@@ -0,0 +1,22 @@
+// Last updated: 1/22/2026, 7:55:40 PM
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int n=nums.size();
+        set<int>st;
+        int ans=0;
+        for(int i=0;i<n;i++){
+           int cnt=0;
+           int j=i;
+            while(st.find(nums[j])==st.end()){
+                st.insert(nums[j]);
+                cnt++;
+                j=nums[j];
+            }
+            ans=max(ans,cnt);
+
+        }
+        return ans;
+        
+    }
+};
