8928edaf7111934a8179b8765cd463556740fa95
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:52:36 +0530

    Create 2320-FindAllKDistantIndicesInAnArray

---
+++ 2320-FindAllKDistantIndicesInAnArray/2320-FindAllKDistantIndicesInAnArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 1/22/2026, 7:52:36 PM
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> ans;
+        int n=nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(abs(i-j)<=k && nums[j]==key){
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
