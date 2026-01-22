df5be2c4385b7797e0cea0d2279bc29e7277459e
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:04 +0530

    Create 878-ShiftingLetters

---
+++ 878-ShiftingLetters/878-ShiftingLetters.cpp
@@ -0,0 +1,12 @@
+// Last updated: 1/22/2026, 7:55:03 PM
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int sum=0;
+        for(int i=shifts.size()-1;i>=0;i--){
+            sum=(sum+shifts[i])%26;
+            s[i]=((s[i]-'a')+sum+26)%26+'a';
+        }
+        return s;
+    }
+};
