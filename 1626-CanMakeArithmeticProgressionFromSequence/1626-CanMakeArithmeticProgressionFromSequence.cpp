34b27ac340c664165ee0bd53e68cdc043a09c704
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:53:22 +0530

    Create 1626-CanMakeArithmeticProgressionFromSequence

---
+++ 1626-CanMakeArithmeticProgressionFromSequence/1626-CanMakeArithmeticProgressionFromSequence.cpp
@@ -0,0 +1,15 @@
+// Last updated: 1/22/2026, 7:53:21 PM
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int d = arr[1] - arr[0];
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i + 1] - arr[i] != d) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
