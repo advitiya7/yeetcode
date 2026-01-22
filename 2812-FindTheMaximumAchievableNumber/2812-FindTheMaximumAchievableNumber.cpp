0f5b00aa1196e114297d20faab65f3c5fabd8add
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:51:52 +0530

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.cpp
@@ -0,0 +1,9 @@
+// Last updated: 1/22/2026, 7:51:51 PM
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        int ans=num+t;
+        return ans+t;
+        
+    }
+};
