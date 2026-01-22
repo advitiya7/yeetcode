766d1cd8ec54adf0b8d09426ec8a0b5753fec692
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:28 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.cpp
@@ -0,0 +1,32 @@
+// Last updated: 1/22/2026, 7:55:28 PM
+class Solution {
+public:
+bool isPalindrome(string s,int i,int j){
+    while(i<=j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        else{
+            i++;
+            j--;
+        }
+    }
+    return true;
+
+}
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+        
+    }
+};
