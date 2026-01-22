cf8f97fe2542a1fb4417a11efcc476b9174c9d40
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:52 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ 442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.cpp
@@ -0,0 +1,19 @@
+// Last updated: 1/22/2026, 7:55:52 PM
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int num=abs(nums[i]);
+            int idx=num-1;
+            if(nums[idx]<0){
+                ans.push_back(num);
+            }
+            else{
+                nums[idx]*=-1;
+            }
+        }
+        return ans;
+    }
+};
