569175be90951e2cbf5de7e9e2378dd9d18c58b2
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:50:40 +0530

    Create 3429-SpecialArrayI

---
+++ 3429-SpecialArrayI/3429-SpecialArrayI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 1/22/2026, 7:50:40 PM
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return true;
+        }
+        for(int i=1;i<n;i++){
+            if(nums[i-1]%2==nums[i]%2){
+                return false;
+            }
+        
+    }
+    return true;
+
+}
+};
