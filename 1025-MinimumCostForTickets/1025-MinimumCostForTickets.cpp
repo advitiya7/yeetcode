3fced4f85e74bbb401387cab0d26b47f0921151f
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:54:41 +0530

    Create 1025-MinimumCostForTickets

---
+++ 1025-MinimumCostForTickets/1025-MinimumCostForTickets.cpp
@@ -0,0 +1,23 @@
+// Last updated: 1/22/2026, 7:54:40 PM
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // dp[i]-> minimum cost of tickets till index i
+        // dp[0]->0
+        set<int> st(days.begin(), days.end());
+        int n = days.size();
+        int size = days[n - 1];
+        vector<int> dp(size + 1, 0);
+        dp[0] = 0;
+        for (int i = 1; i <= size; i++) {
+            if (st.find(i) == st.end()) {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+            dp[i] = INT_MAX;
+            dp[i] = min(costs[0] + dp[(max(0, i - 1))],
+            min(costs[1] + dp[(max(0, i - 7))], costs[2] + dp[(max(0, i - 30))]));
+        }
+        return dp[size];
+    }
+};
