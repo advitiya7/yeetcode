d94e66b123e72d564d7c8d303b34d41b6f3a0b23
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:58 +0530

    Create 437-PathSumIii

---
+++ 437-PathSumIii/437-PathSumIii.cpp
@@ -0,0 +1,38 @@
+// Last updated: 1/22/2026, 7:55:57 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int ans,sum;
+ void solve(TreeNode*root,long long sum,int targetSum){
+    if(root==NULL){
+        return;
+    }
+    sum+=root->val;
+    if(sum==targetSum){
+        ans++;
+    }
+    solve(root->left,sum,targetSum);
+    solve(root->right,sum,targetSum);
+
+ }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root==NULL){
+            return 0;
+        }
+        solve(root,sum,targetSum);
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        return ans;
+        
+    }
+};
