93c6730ab81440281cd132fb53ccc5954fb7a29d
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:54:32 +0530

    Create 1121-PartitionArrayForMaximumSum

---
+++ 1121-PartitionArrayForMaximumSum/1121-PartitionArrayForMaximumSum.cpp
@@ -0,0 +1,29 @@
+// Last updated: 1/22/2026, 7:54:32 PM
+class Solution {
+public:
+int dp[501];
+int f(int i,vector<int>&arr,int k){
+    if(i>=arr.size()){
+        return 0;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int sum=0;
+    int maxi=0;
+    for(int j=i;j<arr.size();j++){
+        maxi=max(maxi,arr[j]);
+        if(j-i+1<=k){
+            sum=max(sum,maxi*(j-i+1)+f(j+1,arr,k));
+        }
+    }
+    return dp[i]=sum;
+}
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n=arr.size();
+        memset(dp,-1,sizeof(dp));
+        return f(0,arr,k);
+
+        
+    }
+};
