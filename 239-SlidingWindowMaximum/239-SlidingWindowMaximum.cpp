8a6ffa53cff76f8f0973308baa7843f788d8a89e
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:56:11 +0530

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -0,0 +1,26 @@
+// Last updated: 1/22/2026, 7:56:11 PM
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            
+            dq.push_back(i);
+            
+            if (dq.front() == i - k) {
+                dq.pop_front();
+            }
+            
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        
+        return ans;
+    }
+};
