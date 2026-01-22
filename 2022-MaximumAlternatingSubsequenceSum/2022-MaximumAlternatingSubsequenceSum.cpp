54bba49399229b72b41fb8a878e2202af80df350
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:52:54 +0530

    Create 2022-MaximumAlternatingSubsequenceSum

---
+++ 2022-MaximumAlternatingSubsequenceSum/2022-MaximumAlternatingSubsequenceSum.cpp
@@ -0,0 +1,14 @@
+// Last updated: 1/22/2026, 7:52:53 PM
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = max(dp[i - 1][1] - nums[i - 1],
+                           dp[i - 1][0]); // 1based indexing
+            dp[i][1] = max(dp[i - 1][0] + nums[i - 1], dp[i - 1][1]);
+        }
+        return max(dp[n][1], dp[n][0]);
+    }
+};
