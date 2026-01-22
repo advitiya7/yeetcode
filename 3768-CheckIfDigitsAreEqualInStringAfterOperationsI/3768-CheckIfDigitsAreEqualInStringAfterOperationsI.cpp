2833b84c4c01faf583c4fdc707c389b00fa7cc48
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:50:11 +0530

    Create 3768-CheckIfDigitsAreEqualInStringAfterOperationsI

---
+++ 3768-CheckIfDigitsAreEqualInStringAfterOperationsI/3768-CheckIfDigitsAreEqualInStringAfterOperationsI.cpp
@@ -0,0 +1,15 @@
+// Last updated: 1/22/2026, 7:50:10 PM
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.size() > 2) {  
+            string k = "";
+            for (int i = 0; i < s.size() - 1; i++) { 
+                int sum = (s[i] - '0' + s[i + 1] - '0') % 10;
+                k += to_string(sum);
+            }
+            s = k;  
+        }
+        return s[0] == s[1];  
+    }
+};
