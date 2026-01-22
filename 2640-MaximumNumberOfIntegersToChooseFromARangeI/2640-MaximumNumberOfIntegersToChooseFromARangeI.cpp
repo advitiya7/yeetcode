a98e70677c04ac3d7160bc506d5780eecd16420b
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:52:08 +0530

    Create 2640-MaximumNumberOfIntegersToChooseFromARangeI

---
+++ 2640-MaximumNumberOfIntegersToChooseFromARangeI/2640-MaximumNumberOfIntegersToChooseFromARangeI.cpp
@@ -0,0 +1,26 @@
+// Last updated: 1/22/2026, 7:52:08 PM
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        set<int>st;
+        int size=banned.size();
+        for(int i=0;i<size;i++){
+            st.insert(banned[i]);
+        }
+        int cnt=0;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(st.find(i)!=st.end()){
+                st.erase(i);
+            }
+            else{
+                ans+=i;
+                if(ans<=maxSum){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+        
+    }
+};
