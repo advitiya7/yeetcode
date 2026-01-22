6e4e990549501d09dcdaa4421dba0240fcaba079
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:56:59 +0530

    Create 72-EditDistance

---
+++ 72-EditDistance/72-EditDistance.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/22/2026, 7:56:58 PM
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // dp[i][j]=minimum no of operation needed to convert string s1 till index i to s2 till index j;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=i+j;
+                }
+               else if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
