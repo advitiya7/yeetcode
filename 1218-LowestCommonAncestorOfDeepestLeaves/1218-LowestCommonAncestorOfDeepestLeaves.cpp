455ab7102efb12f5c2704dbccb8d90550a6e72ca
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:54:22 +0530

    Create 1218-LowestCommonAncestorOfDeepestLeaves

---
+++ 1218-LowestCommonAncestorOfDeepestLeaves/1218-LowestCommonAncestorOfDeepestLeaves.cpp
@@ -0,0 +1,39 @@
+// Last updated: 1/22/2026, 7:54:21 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int depth(TreeNode*root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1+max(depth(root->left),depth(root->right));
+}
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int leftdepth=depth(root->left);
+        int rightdepth=depth(root->right);
+        if(leftdepth==rightdepth){
+            return root;
+        }
+        else if(leftdepth>rightdepth){
+            return lcaDeepestLeaves(root->left);
+        }
+        else{
+            return lcaDeepestLeaves(root->right);
+        }
+
+        
+    }
+};
