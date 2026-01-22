6f1caa0577a7fc22f21d7eeb7ee274e25422f5a3
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:51:57 +0530

    Create 2751-SlidingSubarrayBeauty

---
+++ 2751-SlidingSubarrayBeauty/2751-SlidingSubarrayBeauty.cpp
@@ -0,0 +1,30 @@
+// Last updated: 1/22/2026, 7:51:57 PM
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        int n = nums.size();
+        vector<int> ans;
+        map<int, int> mp;
+        int i = 0;
+        int j = 0;
+        while (j < n) {
+            mp[nums[j]]++;
+            if (j - i + 1 == k) {
+                int cnt = x;
+                for (int k = -50; k <= -1; k++) {
+                    cnt -= mp[k];
+                    if (cnt <= 0) {
+                        ans.push_back(k);
+                        break;
+                    }
+                }
+                if (cnt > 0)
+                 ans.push_back(0);
+                mp[nums[i]]--;
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
