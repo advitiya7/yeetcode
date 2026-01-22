f81a85d607ec7d75ba9903980db80b9ba0f90af0
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:51:44 +0530

    Create 2867-WaysToSplitArrayIntoGoodSubarrays

---
+++ 2867-WaysToSplitArrayIntoGoodSubarrays/2867-WaysToSplitArrayIntoGoodSubarrays.cpp
@@ -0,0 +1,36 @@
+// Last updated: 1/22/2026, 7:51:44 PM
+class Solution {
+public:
+    int dp[100001][2];
+    int MOD = 1e9 + 7;
+    long long getAns(vector<int>& nums, int i, int k) {
+        if (i >= nums.size()) {
+            return k == 1; // if one 1 is there
+        }
+
+        if (dp[i][k] != -1)
+            return dp[i][k];
+
+        if (k == 1) {
+            if (nums[i] ==
+                0) { // then i have two choice eithe break it here or  not
+
+                return dp[i][k] = (getAns(nums, i + 1, 0) % MOD +
+                                   getAns(nums, i + 1, k) % MOD) %
+                                  MOD;
+            } else { // now i have to break it because one 1's in subarray is
+                     // already present
+
+                return dp[i][k] = getAns(nums, i + 1, 1) % MOD;
+            }
+        } else {
+
+            return dp[i][k] = getAns(nums, i + 1, k + nums[i]) % MOD;
+        }
+    }
+    int numberOfGoodSubarraySplits(vector<int>& nums) {
+
+        memset(dp, -1, sizeof(dp));
+        return getAns(nums, 0, 0);
+    }
+};
