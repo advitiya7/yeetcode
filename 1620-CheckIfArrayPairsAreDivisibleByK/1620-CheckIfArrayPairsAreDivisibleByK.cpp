e57140bad9f77b4242d37a3e35c52abeac9b0b3d
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:53:26 +0530

    Create 1620-CheckIfArrayPairsAreDivisibleByK

---
+++ 1620-CheckIfArrayPairsAreDivisibleByK/1620-CheckIfArrayPairsAreDivisibleByK.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/22/2026, 7:53:26 PM
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        int n=arr.size();
+        vector<int>cnt(k,0);
+        for(int i=0;i<n;i++){
+            int rem=((arr[i]%k)+k)%k;// this is done as there arenegative numbers in the array
+            cnt[rem]++;
+        }
+        for(int i=0;i<k;i++){
+            if(i==0){
+                if(cnt[0]%2!=0){
+                    return false;
+                }
+            }
+            else if(cnt[i]!=cnt[k-i]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
