eee18560bb4b633330c685ccc9c2a10a0e522ad6
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:53:43 +0530

    Create 1488-SortIntegersByThePowerValue

---
+++ 1488-SortIntegersByThePowerValue/1488-SortIntegersByThePowerValue.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/22/2026, 7:53:43 PM
+class Solution {
+public:
+    int getKth(int lo, int hi, int k) {
+        vector<pair<int,int>>vp;
+        for(int i=lo;i<=hi;i++){
+            int cnt=0;
+            int num=i;
+            while(num!=1){
+                if(num%2!=0){
+                    num=3*num+1;
+                }
+                else{
+                    num/=2;
+                }
+                cnt++;
+            }
+            vp.push_back({cnt,i});
+        }
+        sort(vp.begin(),vp.end());
+        return vp[k-1].second;
+        
+    }
+};
