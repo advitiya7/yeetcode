4140649aeb638b9928e8bcf4efaf628f39354dbb
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:52:26 +0530

    Create 2443-CheckIfThereIsAValidPartitionForTheArray

---
+++ 2443-CheckIfThereIsAValidPartitionForTheArray/2443-CheckIfThereIsAValidPartitionForTheArray.cpp
@@ -0,0 +1,29 @@
+// Last updated: 1/22/2026, 7:52:25 PM
+class Solution {
+public:
+int dp[100001];
+bool f(int i,vector<int>&nums,int n){
+    if(i>=nums.size()){
+        return true;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    if(i+1<n &&nums[i]==nums[i+1] && f(i+2,nums,n)){
+        return dp[i]=true;
+    }
+    if(i+2<n &&nums[i]==nums[i+1] && nums[i]==nums[i+2] && f(i+3,nums,n)){
+        return dp[i]=true;
+    }
+    if(i+2<n && nums[i]==nums[i+1]-1 && nums[i]==nums[i+2]-2 && f(i+3,nums,n)){
+        return dp[i]=true;
+    }
+    return dp[i]=false;
+}
+    bool validPartition(vector<int>& nums) {
+        int n=nums.size();
+        memset(dp,-1,sizeof(dp));
+        return f(0,nums,n);
+        
+    }
+};
