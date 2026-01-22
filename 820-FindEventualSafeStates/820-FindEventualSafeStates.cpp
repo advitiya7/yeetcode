5451bb1c944d6bc9d4230ecb7bfa87b08da4a502
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:18 +0530

    Create 820-FindEventualSafeStates

---
+++ 820-FindEventualSafeStates/820-FindEventualSafeStates.cpp
@@ -0,0 +1,27 @@
+// Last updated: 1/22/2026, 7:55:17 PM
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        map<int,int> indeg;
+        queue<int> q;
+        vector<int> adj[n],ans;
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                adj[it].push_back(i);
+                indeg[i]++;
+            }
+        }
+        for(int i=0;i<n;i++) if(indeg[i]==0) q.push(i);
+        while(!q.empty()){
+            int node = q.front();q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0) q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
