effec4f15246bfff5442b681c6c1af1cc9f8493d
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:52:14 +0530

    Create 2551-ApplyOperationsToAnArray

---
+++ 2551-ApplyOperationsToAnArray/2551-ApplyOperationsToAnArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 1/22/2026, 7:52:13 PM
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        return nums;
+    }
+};
