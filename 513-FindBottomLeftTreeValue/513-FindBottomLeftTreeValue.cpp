aa9526ca00a882051056fa04f10d9ebfdc66d673
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:51 +0530

    Create 513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.cpp
@@ -0,0 +1,32 @@
+// Last updated: 1/22/2026, 7:55:50 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        int val=0;
+        while(!q.empty()){
+            TreeNode*temp=q.front();
+            q.pop();
+            val=temp->val;
+            if(temp->right!=NULL){
+                q.push(temp->right);
+            }
+             if(temp->left!=NULL){
+                q.push(temp->left);
+            }
+        }
+        return val;
+    }
+};
