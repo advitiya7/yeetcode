8cf65e74c486cb0909c3fc681ab812429ea915b5
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:57:33 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.cpp
@@ -0,0 +1,36 @@
+// Last updated: 1/22/2026, 7:57:33 PM
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        bool is_one=false;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                is_one=true;
+            }
+            if(nums[i]<=0 || nums[i]>n){
+                nums[i]=1;
+            }
+        }
+        if(!is_one){
+            return 1;
+        }
+        for(int i=0;i<n;i++){
+            int num=abs(nums[i]);
+            int idx=num-1;
+            if(nums[idx]<0){
+                continue;
+            }
+                nums[idx]*=-1;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        return n+1;
+
+
+        
+    }
+};
