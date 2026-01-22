03b059ffd2d2a210ef16e532743bbd2e60e672eb
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:55:22 +0530

    Create 658-FindKClosestElements

---
+++ 658-FindKClosestElements/658-FindKClosestElements.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/22/2026, 7:55:22 PM
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>>pq;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int diff=abs(arr[i]-x);
+            pq.push({diff,arr[i]});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int>res;
+        while(!pq.empty()){
+            auto top=pq.top();
+            res.push_back(top.second);
+            pq.pop();
+        }
+        sort(res.begin(),res.end());
+        return res;
+        
+    }
+};
