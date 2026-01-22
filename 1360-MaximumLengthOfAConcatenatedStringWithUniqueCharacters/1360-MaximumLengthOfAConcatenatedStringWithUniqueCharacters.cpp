93fd2ff451d91b2fe01d2c1417e616b0f1366489
Advitiya Singh <98165017+advitiya7@users.noreply.github.com>
2026-01-22 19:54:01 +0530

    Create 1360-MaximumLengthOfAConcatenatedStringWithUniqueCharacters

---
+++ 1360-MaximumLengthOfAConcatenatedStringWithUniqueCharacters/1360-MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp
@@ -0,0 +1,32 @@
+// Last updated: 1/22/2026, 7:54:00 PM
+class Solution {
+public:
+int maxi=INT_MIN;
+bool check(string &s){
+    set<char>st;
+    for(auto it:s){
+        st.insert(it);
+    }
+    return st.size()==s.size();
+}
+int solve(int i,vector<string>& arr,int n,string temp){
+   
+    if(check(temp)==false){
+        return 0;
+    }
+    if(i>=n){
+        int size=temp.size();
+        maxi=max(size,maxi);
+        return maxi;
+    }
+int take=solve(i+1,arr,n,temp+arr[i]);
+int notake=solve(i+1,arr,n,temp);
+return maxi;
+}
+    int maxLength(vector<string>& arr) {
+        int n=arr.size();
+        return solve(0,arr,n,"");
+        // return maxi;
+        
+    }
+};
